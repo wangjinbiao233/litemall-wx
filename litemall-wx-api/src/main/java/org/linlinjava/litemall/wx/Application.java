c6ab2f344ac3cf9f741f00acd385c61c394c4ffa
shaoyan <shaoyan@dgtis.com>
2018-08-02 11:29:12 +0800

    test

--- litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/Application.java
+++ litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/Application.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("org.linlinjava.litemall.db.dao")
 public class Application {
 
+	//test
     public static void main(String[] args) {
     	
         SpringApplication.run(Application.class, args);
